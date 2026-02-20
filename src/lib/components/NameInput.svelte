ed31fd5099edb2e95c28458bb7f861b3d62aefc2
heng.vicheka <h.h.vicheka@gmail.com>
2026-02-20 13:54:56 +0700

    feat: create home page (without css)

---
+++ src/lib/components/NameInput.svelte
@@ -0,0 +1,5 @@
+<form method="POST" action="?/saveName">
+    <p>Hi there, what's your name?</p>
+    <input name="username" type="text" placeholder="Enter your name..." />
+    <button type="submit">Save</button>
+</form>
