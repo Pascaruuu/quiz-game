cf35e26f64a4a24117d33138e9a4f12bca0015d5
ENG Chanveasna <2023488eng@aupp.edu.kh>
2026-02-20 22:15:54 +0700

    feat: create db connection and wrap with drizzle and create db query functions

---
+++ src/lib/server/db_controller.ts
@@ -0,0 +1,15 @@
+import { db } from './db';
+import { scoreboard } from '$lib/db/schema';
+import { desc } from 'drizzle-orm';
+
+// Get Top 10
+export async function getLeaderboard() {
+    return await db.select()
+        .from(scoreboard)
+        .orderBy(desc(scoreboard.score))
+        .limit(10);
+}
+
+export async function saveScore(username: string, score: number) {
+    return await db.insert(scoreboard).values({ username, score });
+}
