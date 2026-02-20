d5445095e7a2369da33da55676a593ddb76df113
ENG Chanveasna <2023488eng@aupp.edu.kh>
2026-02-20 21:48:02 +0700

    feat: database migration with drizzle ORM

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,17 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+
+export const scoreboard = sqliteTable("scoreboard", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  username: text("username").notNull(),
+  score: integer("score").notNull(),
+  createdAt: text("createdAt").default(sql`(DATETIME('now'))`).notNull(),
+});
+
+// export const dictionary = sqliteTable("dictionary", {
+//   id: integer("id").primaryKey({ autoIncrement: true }),
+//   question: text("question").notNull().unique(),
+//   answer: text("answer").notNull(),
+//   createdAt: text("createdAt").default(sql`(DATETIME('now'))`).notNull(),
+//   modifiedAt: text("modifiedAt").default(sql`(DATETIME('now'))`).notNull(),
+// });
